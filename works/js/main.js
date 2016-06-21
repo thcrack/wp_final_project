f9c5b5038f0a438edd68043acf0a673a1d986bd4
thcrack <kutar7580@gmail.com>
2016-06-21 22:47:10 +0800

    designer modal

--- works/js/main.js
+++ works/js/main.js
@@ -22,15 +22,47 @@ setTimeout(function(){
 },2000);
 
 var itemData = firebase.database().ref("items");
+var userData = firebase.database().ref("designers");
 
 itemData.once("value",function(input){
     console.log("once");
     input.forEach(function(childIn){
         var entry = childIn.val();
-        console.log(childIn.val());
         imgRoot.child("items/"+ entry.userUID + "/" + entry.itemID + "/itemPic.jpg").getDownloadURL().then(function(picUrl){
 
             $('#designer-view').append('<div class="designer-view-block" id="' + entry.itemID + '"></div>');
+
+            $('#' + entry.itemID).click(function(){
+                $('#work-view-modal').empty();
+                $('#work-view-modal').append('<div class="modal-dialog" id="work-dialog"></div>');
+                $('#work-dialog').append('<div class="modal-content" id="work-content"></div>');
+                $('#work-content').append('<div class="modal-body" id="work-body"></div>');
+
+                $('#work-body').append('<div id="work-pic"></div>');
+                $('#work-body').append('<div id="work-designer"></div>');
+
+                $('#work-pic').css('background-image','url(' + picUrl + ')');
+
+                firebase.database().ref("designers/" + entry.userUID).once("value",function(userIn){
+
+                    var author = userIn.val();
+
+                    imgRoot.child("profile/"+ entry.userUID + "/profilePic.jpg").getDownloadURL().then(function(inUrl){
+
+                        $('#work-designer').append('<div id="work-designer-pic">');
+                        $('#work-designer-pic').css('background-image','url(' + inUrl + ')');
+
+                        $('#work-designer').append('<div id="work-designer-name">' + author.userName + '</div>');
+                        $('#work-designer').append('<div id="work-designer-location">' + author.userLocation + '</div>');
+                        $('#work-designer').append('<div id="work-designer-phone">' + author.userPhone + '</div>');
+                        $('#work-designer').append('<div id="work-designer-desc">' + author.userDesc + '</div>');
+
+                    });
+
+                });
+
+                $('#work-view-modal').modal('show');
+            });
 
             $('#' + entry.itemID).append('<h4>' + entry.itemStyle + '</h4>');
             $('#' + entry.itemID).css('background-image','url(' + picUrl + ')');
