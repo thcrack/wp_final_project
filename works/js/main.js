f86efcfe5bbef21173247ca84b923ac87fc9314e
thcrack <kutar7580@gmail.com>
2016-06-22 10:57:33 +0800

    add filters, pagination

--- works/js/main.js
+++ works/js/main.js
@@ -14,58 +14,211 @@ var items = firebase.database().ref("items");
 
 var imgRoot = firebase.storage().ref();
 
-$('#designer-view').hide();
-
-setTimeout(function(){
-    $('#designer-view').show();
-    $('#designer-view-loading').hide();
-},2000);
+var currentPage = 1;
+var ITEMS_PER_PAGE = 16;
 
 var itemData = firebase.database().ref("items");
 var userData = firebase.database().ref("designers");
 
-itemData.limitToFirst(24).once("value",function(input){
-    console.log("once");
-    input.forEach(function(childIn){
-        var entry = childIn.val();
-        imgRoot.child("items/"+ entry.userUID + "/" + entry.itemID + "/itemPic.jpg").getDownloadURL().then(function(picUrl){
+var itemCacheArray = new Array();
+var dataCount = 0;
+var pageCount = 0;
 
-            $('#designer-view').append('<div class="designer-view-block" id="' + entry.itemID + '"></div>');
+getData(itemData);
+buildFilter();
 
-            $('#' + entry.itemID).click(function(){
-                $('#work-view-modal').empty();
-                $('#work-view-modal').append('<div class="modal-dialog" id="work-dialog"></div>');
-                $('#work-dialog').append('<div class="modal-content" id="work-content"></div>');
-                $('#work-content').append('<div class="modal-body" id="work-body"></div>');
+function buildFilter(){
 
-                $('#work-body').append('<div id="work-pic"></div>');
-                $('#work-body').append('<div id="work-designer"></div>');
+	//regions
 
-                $('#work-pic').css('background-image','url(' + picUrl + ')');
+	firebase.database().ref("regions").orderByKey().once("value",function(regionList){
+		regionList.forEach(function(sRegion){
+			createFilterRegionDOM(sRegion);
+		});
+	});
 
-                firebase.database().ref("designers/" + entry.userUID).once("value",function(userIn){
+	//styles
 
-                    var author = userIn.val();
+	createFilterStyleDOM();
 
-                    imgRoot.child("profile/"+ entry.userUID + "/profilePic.jpg").getDownloadURL().then(function(inUrl){
+	//designers
 
-                        $('#work-designer').append('<div id="work-designer-pic">');
-                        $('#work-designer-pic').css('background-image','url(' + inUrl + ')');
+	firebase.database().ref("designers").orderByKey().once("value",function(designerList){
+		designerList.forEach(function(sDesigner){
+			createFilterDesignerDOM(sDesigner);
+		});
+	});
 
-                        $('#work-designer').append('<div id="work-designer-name">' + author.userName + '</div>');
-                        $('#work-designer').append('<div id="work-designer-location">' + author.userLocation + '</div>');
-                        $('#work-designer').append('<div id="work-designer-phone">' + author.userPhone + '</div>');
-                        $('#work-designer').append('<div id="work-designer-desc">' + author.userDesc + '</div>');
+}
 
-                    });
+var rIndex = 0;
+
+function createFilterRegionDOM(r){
+
+	var rName = r.val().name;
+	$('#filter-region-list').append('<li><a href="#designer-view-loading" value="' + rName + '">' + rName + '</a></li>');
+	$('#filter-region-list a[value="' + rName + '"]').click(function(){
+		firebase.database().ref("designers").orderByChild('userLocation').startAt(rName).endAt(rName).once("value",function(rdList){
+			toggleLoading(true);
+			itemCacheArray = new Array();
+			rIndex = 0;
+			dataCount = 0;
+			rdList.forEach(function(input){
+				loadPortfolio(input);
+			});
+		});
+	});
+};
+
+function loadPortfolio(input){
+	var dID = input.val().userUID;
+	firebase.database().ref("portfolio/" + dID).once("value", function(wInput){
+		dataCount += wInput.numChildren();
+	    wInput.forEach(function(childIn){
+	    	console.log(childIn.val());
+	        itemCacheArray.push(childIn.val());
+	        rIndex ++;
+	        if(rIndex==dataCount){
+	        	pageCount = Math.ceil(dataCount/ITEMS_PER_PAGE);
+	        	getNewPage(1);
+	        	currentPage = 1;
+	        }
+	    });
+	});
+}
+
+function createFilterStyleDOM(){
+
+	$('#filter-style-list a').click(function(){
+		getData(firebase.database().ref("style_catalog/" + $(this).attr('value')));
+	})
+
+};
+
+function createFilterDesignerDOM(r){
+
+	var rName = r.val().userName;
+	$('#filter-designer-list').append('<li><a href="#designer-view-loading" value="' + rName + '">' + rName + '</a></li>');
+	$('#filter-designer-list a[value="' + rName + '"]').click(function(){
+		getData(firebase.database().ref("portfolio/" + r.val().userUID));
+	})
+
+};
+
+function buildPagination(){
+	$('#designer-view-paging').empty();
+	$('#designer-view-paging').append('<ul class="pagination pagination-lg" id="paging-bar"></ul>');
+	for(var i = 0; i < pageCount; i++){
+		var page = i + 1;
+		if(currentPage == page){
+			$('#paging-bar').append('<li class="active"><a>' + parseInt(page) + '</a></li>');
+		}else{
+			$('#paging-bar').append('<li><a href="#designer-view-loading" page="' + parseInt(page) + '">' + parseInt(page) + '</a></li>');
+		}
+		$('#paging-bar a[page="' + parseInt(page) + '"]').click(function(){
+			currentPage = parseInt($(this).attr('page'));
+			getNewPage(currentPage);
+			console.log("fired");
+		});
+	}
+}
+
+function getData(data){
+	toggleLoading(true);
+	data.once("value",function(input){
+		var index = 0;
+		itemCacheArray = new Array();
+		dataCount = input.numChildren();
+		pageCount = Math.ceil(dataCount/ITEMS_PER_PAGE);
+	    input.forEach(function(childIn){
+	        itemCacheArray.push(childIn.val());
+	        index ++;
+	        if(index==dataCount){
+	        	getNewPage(1);
+	        	currentPage = 1;
+	        }
+	    });
+	});
+}
+
+function toggleLoading(boo){
+	if(boo){
+		$('#designer-view-paging').hide();
+		$('#designer-view').hide();
+		$('#designer-view-loading').show();
+	}else{
+		$('#designer-view').show();
+	    $('#designer-view-paging').show();
+	    $('#designer-view-loading').hide();
+	}
+}
+
+function getNewPage(pageNum){
+
+	toggleLoading(true);
+
+	setTimeout(function(){
+	    
+		toggleLoading(false);
+
+	},2000);
+
+	loadDataFromArray(pageNum);
+
+}
+
+function loadDataFromArray(page){
+
+	$('#designer-view').empty();
+	console.log(itemCacheArray);
+
+	for(i = ITEMS_PER_PAGE*(page-1); i < ITEMS_PER_PAGE*page; i++){
+		if(i >= itemCacheArray.length) break;
+		var sItem = itemCacheArray[i];
+	    createDOM(sItem);
+	}
+
+	buildPagination();
+}
+
+function createDOM(entry){
+	imgRoot.child("items/"+ entry.userUID + "/" + entry.itemID + "/itemPic.jpg").getDownloadURL().then(function(picUrl){
+
+        $('#designer-view').append('<div class="designer-view-block" id="' + entry.itemID + '"></div>');
+
+        $('#' + entry.itemID).click(function(){
+            $('#work-view-modal').empty();
+            $('#work-view-modal').append('<div class="modal-dialog" id="work-dialog"></div>');
+            $('#work-dialog').append('<div class="modal-content" id="work-content"></div>');
+            $('#work-content').append('<div class="modal-body" id="work-body"></div>');
+
+            $('#work-body').append('<div id="work-pic"></div>');
+            $('#work-body').append('<div id="work-designer"></div>');
+
+            $('#work-pic').css('background-image','url(' + picUrl + ')');
+
+            firebase.database().ref("designers/" + entry.userUID).once("value",function(userIn){
+
+                var author = userIn.val();
+
+                imgRoot.child("profile/"+ entry.userUID + "/profilePic.jpg").getDownloadURL().then(function(inUrl){
+
+                    $('#work-designer').append('<div id="work-designer-pic">');
+                    $('#work-designer-pic').css('background-image','url(' + inUrl + ')');
+
+                    $('#work-designer').append('<div id="work-designer-name">' + author.userName + '</div>');
+                    $('#work-designer').append('<div id="work-designer-location">' + author.userLocation + '</div>');
+                    $('#work-designer').append('<div id="work-designer-phone">' + author.userPhone + '</div>');
+                    $('#work-designer').append('<div id="work-designer-desc">' + author.userDesc + '</div>');
 
                 });
 
-                $('#work-view-modal').modal('show');
             });
 
-            $('#' + entry.itemID).append('<h4>' + entry.itemStyle + '</h4>');
-            $('#' + entry.itemID).css('background-image','url(' + picUrl + ')');
-        })
-    });
-});
+            $('#work-view-modal').modal('show');
+        });
+
+        $('#' + entry.itemID).append('<h4>' + entry.itemStyle + '</h4>');
+        $('#' + entry.itemID).css('background-image','url(' + picUrl + ')');
+    })
+}
